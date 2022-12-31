96259522027e71b7db27b136e490bda6585d69b6
= <shawaarma0200@gmail.com>
2022-12-31 19:58:35 +0300

    netlfy

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 dotenv.config();
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://saanyoapp.netlify.app");
+  res.header("Access-Control-Allow-Origin", "https://animlot.netlify.app/");
   res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: ["https://saanyoapp.netlify.app", "http://localhost:3000"],
+    origin: ["https://animlot.netlify.app/", "http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
