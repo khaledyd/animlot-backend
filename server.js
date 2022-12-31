9d5f5c847547df8dde39a9447c17761e32ed3d26
= <shawaarma0200@gmail.com>
2022-12-31 20:22:46 +0300

    cors again

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: ["https://animlot.netlify.app", "http://localhost:3000"],
+    origin: "https://animlot.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
