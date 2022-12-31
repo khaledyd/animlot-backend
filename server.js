8a667592887443d88f45dc1bcafcf6a17704c1bb
= <shawaarma0200@gmail.com>
2022-12-31 19:26:49 +0300

    some cors

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import videoRoutes from "./routes/videos.js";
 import commentRoutes from "./routes/comments.js";
 import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 
 
@@ -13,8 +14,21 @@ import cookieParser from "cookie-parser";
 const app = express();
 dotenv.config();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://saanyoapp.netlify.app");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 
+app.use(
+  cors({
+    origin: ["https://saanyoapp.netlify.app", "http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 
 
